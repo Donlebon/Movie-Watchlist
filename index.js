da6b2bcdb25ae946d137ed5049084a93acba9f29
Don <donducdo@gmail.com>
2022-08-05 20:32:01 -0700

    Testing JS file

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ let exploreMessage = document.querySelector(".explore-message")
 
 let searchList = document.querySelector(".searchlist")
 
+alert("Hello")
 
 searchBar.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
